928f1328d0ba0a82d2401f1d221a7d12ddce0441
HanGyeongjun <030212hgjun@gmail.com>
2023-11-08 02:39:23 +0900

    [Init] #1 - Set project files
    - Set project files
    - Modify .gitignore file (delete SwiftPM)

---
+++ DuriThreeMeal/DuriThreeMeal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DuriThreeMeal
+//
+//  Created by HanGyeongjun on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DuriThreeMeal/DuriThreeMeal/DuriThreeMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuriThreeMealApp.swift
+//  DuriThreeMeal
+//
+//  Created by HanGyeongjun on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DuriThreeMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
